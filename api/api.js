6bfe852286283d749c398c9b00d976cf8e6752a8
Jonagarme <garciajonathan268@gmail.com>
2020-11-17 12:20:10 -0500

    actualziando

--- api/api.js
+++ api/api.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const port = 3000;
 
-app.get("*", (req, res) => {
-  console.log("hola mundo!");
-  res.send({ mensaje: "Chancho feo" });
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-module.exports = app;
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
